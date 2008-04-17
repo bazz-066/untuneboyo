ff215be6737924b2087a2568e1f631bafad5e3e7
BazZ.04 <BazZ.04@423884b3-e54a-0410-964e-2bb93712a51a>
2008-04-17 12:29:27 +0000

    nambahin class bwt koneksi ke server bwt dpet network info

---
+++ Untune Boyo/src/untuneboyo/connection/NetworkInfoConnector.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package untuneboyo.connection;
+
+import javax.microedition.io.SocketConnection;
+
+/**
+ *
+ * @author baskoro
+ */
+public class NetworkInfoConnector 
+{
+    private SocketConnection scSocket;
+}
